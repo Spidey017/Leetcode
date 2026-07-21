2de1b8aa041a16ad1ddfeb763e3b5afd299cf424
Arman Attar <ayanattar49@gmail.com>
2026-07-21 20:02:59 +0530

    Time: 447 ms (10.72%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1452-restaurant-growth/restaurant-growth.sql
@@ -0,0 +1,25 @@
+# Write your MySQL query statement below
+WITH daily AS
+(
+    SELECT
+        visited_on,
+        SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+)
+
+SELECT
+    visited_on,
+    SUM(amount) OVER (
+        ORDER BY visited_on
+        ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+    ) AS amount,
+    ROUND(
+        SUM(amount) OVER (
+            ORDER BY visited_on
+            ROWS BETWEEN 6 PRECEDING AND CURRENT ROW
+        ) / 7,
+        2
+    ) AS average_amount
+FROM daily
+LIMIT 6, 1000000;
