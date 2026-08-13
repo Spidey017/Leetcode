9e04245520da6714e2ff2719e53ac672e9f41fde
Arman Attar <ayanattar49@gmail.com>
2026-08-13 23:12:43 +0530

    Time: 2 ms (25.91%) | Memory: 19.4 MB (7.93%) - LeetSync

---
+++ 1698-replace-all-s-to-avoid-consecutive-repeating-characters/replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def modifyString(self, s: str) -> str:
+
+        s = list(s)
+
+        for i in range(len(s)):
+
+            if s[i] == '?':
+                
+                for ch in "abc":
+                    if i > 0 and s[i-1] == ch:
+                        continue
+
+                    if i < len(s)-1 and s[i+1] == ch:
+                        continue
+
+                    s[i] = ch
+                    break
+
+        return "".join(s)
